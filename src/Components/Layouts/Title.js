68ff0aefb841f076827e074c0d299b1fe468cdca
srinu <doddaeswararao@gmail.com>
2025-01-02 22:31:33 +0530

    conflicts

--- src/Components/Layouts/Title.js
+++ src/Components/Layouts/Title.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 const Title = ({ title, des }) => {
   return (
@@ -11,17 +10,3 @@ const Title = ({ title, des }) => {
   );
 };
 export default Title;
-=======
-import React from "react";
-const Title = ({ title, des }) => {
-  return (
-    <div className="flex flex-col gap-4 font-titleFont">
-      <h3 className="text-1xl uppercase font-light text-designColor tracking-wide">
-        {title}
-      </h3>
-      <h1 className="text-5xl text-gray-300 font-bold capitalize">{des}</h1>
-    </div>
-  );
-};
-export default Title;
->>>>>>> 4c9ffb74b9cd9218e91213e6033cfab1121aadd9
